7a93ae1fe487952d451c086a314337ed25afd371
korolevgena <korolevgena@rambler.ru>
2016-02-26 14:50:21 +0300

    nginx install + my conf

--- create_dirs_in_web.sh
+++ create_dirs_in_web.sh
@@ -2,4 +2,9 @@ mkdir public/img -p
 mkdir public/js -p
 mkdir public/css -p
 mkdir uploads
-mkdir etc
+
+sudo apt-get update 
+sudo apt-get install nginx
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo restart service nginx
