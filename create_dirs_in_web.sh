768a20eb5ada8b6246ef8bffd889cb36b0dc7cb9
korolevgena <korolevgena@rambler.ru>
2016-02-26 14:53:29 +0300

    restart fix

--- create_dirs_in_web.sh
+++ create_dirs_in_web.sh
@@ -7,4 +7,4 @@ sudo apt-get update
 sudo apt-get install nginx
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo restart service nginx
+sudo service nginx restart
