c26ad9f70a91f8982607fc35935eb1e9bf5212d6
korolevgena <korolevgena@rambler.ru>
2016-03-02 14:02:05 +0300

    inst & conf supervisor

--- create_dirs_in_web.sh
+++ create_dirs_in_web.sh
@@ -5,6 +5,8 @@ mkdir uploads
 
 sudo apt-get update 
 sudo apt-get install nginx
+sudo apt-get install supervisor 
+sudo cp /home/box/web/project.conf /etc/supervisor/conf.d/project.conf
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo service nginx restart
