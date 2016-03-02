a72d1165547733e27da0c47a5c0acc2054418529
korolevgena <korolevgena@rambler.ru>
2016-03-02 16:34:25 +0300

    .

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
-def hello(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello!"]
+from cgi import parse_qs
+
+def hello(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+	# Returns a dictionary containing lists as values
+	args = parse_qs(environ['QUERY_STRING'])
+	x = args.get('x', [''])[0] # Returns the X value.
+	y = args.get('y', [''])[0] # Returns the Y value.
+	response_body = 'x=' + x + 'y=' + y
+    return [response_body]
