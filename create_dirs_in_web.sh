611f2eb8b9780ed5e80f0d85d1b1f02e44c6a8b5
korolevgena <korolevgena@rambler.ru>
2016-03-03 11:24:38 +0300

    .

--- create_dirs_in_web.sh
+++ create_dirs_in_web.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install nginx
 sudo apt-get install supervisor 
 sudo cp /home/box/web/project.conf /etc/supervisor/conf.d/project.conf
-sudo cp etc/gunicorn.py /etc/gunicorn.d/gunicorn.py 
+sudo cp /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo service nginx restart
