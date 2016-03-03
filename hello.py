ae808bfeb72f7b3053c2ac9038489a3f88d03ce6
korolevgena <korolevgena@rambler.ru>
2016-03-03 15:56:50 +0300

    .

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 def hello(environ, start_response):
-	status = '200 OK'
 	start_response('200 OK', [('Content-Type', 'text/plain')])
 	body = "\r\n".join(environ['QUERY_STRING'].split("&")) 
 	return [ body ]
