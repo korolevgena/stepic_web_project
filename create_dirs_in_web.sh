38a3da84dae596cf18f61c26f9a36104422de392
korolevgena <korolevgena@rambler.ru>
2016-03-10 10:40:05 +0300

    add django init

--- create_dirs_in_web.sh
+++ create_dirs_in_web.sh
@@ -10,3 +10,7 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo service nginx restart
+cd /bome/box/web
+django-admin startproject ask
+cd /bome/box/web/ask
+./manage.py startapp qa
